acb8e62cb20d341fcd60d8595060080e6fce44c8
cristhian rodriguez <crisdrodriguezm@hotmail.com>
2018-09-24 16:35:18 -0500

    codigo vacico de JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
-console.log("hola mundo");
+var numero1;
+var numero2 = 10.5;
+var resultado = numero1*numero2;
+var nombre = "Cristhian Rodriguez";
+numero1 = 5;
+numero1*=numero2;
+var arreglo = ["as","bs","cs","ds","es"];
+console.log("hola mundo",numero1,numero2,nombre);
+function iva(precio){
+return precio*0.19;
+};
+console.log(iva(100));
